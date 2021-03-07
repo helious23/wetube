db13c00921284b379c819784549903aabd00531c
Max16 <max16@naver.com>
2021-03-07 11:15:08 +0900

    #2.25 More Controllers

--- middlewares.js
+++ middlewares.js
@@ -4,6 +4,10 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
+  res.locals.user = { // DB 생성 전, 가짜 user  정보를 변수로 입력 
+    isAuthenticated : true,
+    id : 1,
+  }
   next(); // app.js 에서 다음 함수인 route 로 넘어가기 위해 필수
 };
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,23 @@
 import express from "express";
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  logout,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
