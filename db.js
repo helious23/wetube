b5eb4718cabf592f1c1c24cd38d67b3b4e60917a
Max16 <max16@naver.com>
2021-03-08 08:40:59 +0900

    #3.2 Configuring Dot Env

--- db.js
+++ db.js
@@ -1,50 +1,20 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL, 
   {
-    id: 324393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Max",
-      email: "max@max.com",
-    },
-  },
-  {
-    id: 121212,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Max",
-      email: "max@max.com",
-    },
-  },
-  {
-    id: 23232323,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Max",
-      email: "max@max.com",
-    },
-  },
-  {
-    id: 111111,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Max",
-      email: "max@max.com",
-    },
-  },
-];
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError)
