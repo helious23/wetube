1a7aad8326176e4d4f9c3f952d227487cea84cc8
Max16 <max16@naver.com>
2021-03-09 09:17:51 +0900

    #3.10 Deleting a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -35,21 +35,51 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = async(req, res) => {
+export const videoDetail = async (req, res) => {
   const {
-    params: {id}
+    params: { id },
+  } = req; // req.params.id 와 동일한 표현
+  try {
+    const video = await Video.findById(id);
+    // async await, mongoose 의 findById method : mongoose 의 id 로 Video model 에서 검색하여 결과 찾은 후 render 실행
+    res.render("videoDetail", { pageTitle: video.title, video }); // videoDetail.pug 로 video 변수로 넘겨줌
+  } catch (error) {
+    res.redirect(routes.home); // error 발생시 홈화면으로 redirect
+  }
+};
+
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id },
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video }); // editVideo template 으로 video object 전송
   } catch (error) {
     res.redirect(routes.home);
   }
 };
-  
 
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description },
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description }); // id로 title, description 검색하여 update
+    // 저장할 필요는 없으므로 함수만 설정
+    res.redirect(routes.videoDetail(id)); // videoDetail 로 redirect
+  } catch (error) {
+    res.redirect(routes.home); // middleware 사용하여 error 발생시 home으로 redirect 하게 할 수 있음(참고)
+  }
+};
 
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
+};
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,20 +1,29 @@
 import express from "express";
 import {
   videoDetail,
-  editVideo,
   deleteVideo,
   getUpload,
   postUpload,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoController";
 import routes from "../routes";
-import {uploadVideo} from "../middlewares";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload); // uploadVideo middleware 추가
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
