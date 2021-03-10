870bd3c7dc5aee2e05fb155f83a6b384c791b4ab
Max16 <max16@naver.com>
2021-03-10 15:09:59 +0900

    #6.1 Local Authentication with Passport part One

--- init.js
+++ init.js
@@ -1,9 +1,12 @@
-import "./db"
-import app from "./app";
+import "./db";
 import dotenv from "dotenv";
+import app from "./app";
+
 dotenv.config(); // key 를 공개적으로 올리지 않게 함 .env 파일에서 로드해서 사용
+
 import "./models/Video";
-import "./models/Comment"
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
