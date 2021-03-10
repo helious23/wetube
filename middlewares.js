2868a17389bc9d70bba6d32fed1e65686626b94c
Max16 <max16@naver.com>
2021-03-10 13:51:19 +0900

    #5 Styling

--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,17 @@
+import multer from "multer";
 import routes from "./routes";
 // local 변수를 global 하게 사용할 수 있게 만듦
-import multer from "multer";
 
-const multerVideo = multer({dest:"uploads/videos/"}) // server 에 있는 video folder
+const multerVideo = multer({ dest: "uploads/videos/" }); // server 에 있는 video folder
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
-  res.locals.user = { // DB 생성 전, 가짜 user  정보를 변수로 입력 
-    isAuthenticated : true,
-    id : 1,
-  }
+  res.locals.user = {
+    // DB 생성 전, 가짜 user  정보를 변수로 입력
+    isAuthenticated: false,
+    id: 1,
+  };
   next(); // app.js 에서 다음 함수인 route 로 넘어가기 위해 필수
 };
 
