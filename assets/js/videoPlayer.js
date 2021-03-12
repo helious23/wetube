68b01a04958808f84c8d4f1bfc78618c4c52f96d
Max16 <max16@naver.com>
2021-03-12 16:59:54 +0900

    #8.6 Volume Bar part Two

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,6 +5,7 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
 
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
@@ -20,9 +21,11 @@ const handleVolumeClick = () => {
   if (videoPlayer.muted) {
     videoPlayer.muted = false;
     volumeBtn.innerHTML = '<i class = "fas fa-volume-up"></i>';
+    volumeRange.value = videoPlayer.volume;
   } else {
     videoPlayer.muted = true;
     volumeBtn.innerHTML = '<i class = "fas fa-volume-mute"></i>';
+    volumeRange.value = 0;
   }
 };
 
@@ -90,13 +93,29 @@ const handleEnded = () => {
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 };
 
+const handleDrag = (event) => {
+  const {
+    target: { value },
+  } = event;
+  videoPlayer.volume = value;
+  if (value >= 0.6) {
+    volumeBtn.innerHTML = '<i class = "fas fa-volume-up"></i>';
+  } else if (value >= 0.2) {
+    volumeBtn.innerHTML = '<i class = "fas fa-volume-down"></i>';
+  } else {
+    volumeBtn.innerHTML = '<i class = "fas fa-volume-off"></i>';
+  }
+};
+
 const init = () => {
+  videoPlayer.volume = 0.5;
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
   videoPlayer.addEventListener("loadedmetadata", setTotalTime);
   videoPlayer.addEventListener("timeupdate", getCurrentTime);
   videoPlayer.addEventListener("ended", handleEnded);
+  volumeRange.addEventListener("input", handleDrag);
 };
 
 if (videoContainer) {
