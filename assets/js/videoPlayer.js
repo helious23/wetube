a10e5e97af8700022d36cc43b276dc1f2bfb3e07
Max16 <max16@naver.com>
2021-03-12 13:17:04 +0900

    #8.2 Mute Unmuite

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,31 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
-const playButton = document.getElementById("jsPlayButton");
+const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  }
+};
+
+const handleVolumeClick = () => {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class = "fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class = "fas fa-volume-mute"></i>';
   }
 };
 
 const init = () => {
-  playButton.addEventListener("click", handlePlayClick);
+  playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 };
 
 if (videoContainer) {
