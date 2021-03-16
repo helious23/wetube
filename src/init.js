03a365020eb0383238685008ba2455494eab2a38
Max16 <max16@naver.com>
2021-03-16 16:16:13 +0900

    nodemon

--- src/init.js
+++ src/init.js
@@ -2,12 +2,11 @@ import "@babel/polyfill";
 import "./db";
 import dotenv from "dotenv";
 import app from "./app";
-
-dotenv.config(); // key 를 공개적으로 올리지 않게 함 .env 파일에서 로드해서 사용
-
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
+
+dotenv.config(); // key 를 공개적으로 올리지 않게 함 .env 파일에서 로드해서 사용
 
 const PORT = process.env.PORT || 4000;
 
