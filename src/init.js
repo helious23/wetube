1a7edca98ef3e28401c2f47373582cb2d1e72628
Max16 <max16@naver.com>
2021-03-17 18:23:55 +0900

    host edit

--- src/init.js
+++ src/init.js
@@ -12,4 +12,4 @@ const PORT = process.env.PORT || 5000;
 
 const handleListening = () =>
   console.log(`✅Listening on: http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
+app.listen(PORT, "0.0.0.0", handleListening);
